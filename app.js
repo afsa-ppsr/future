f4ad365af7fa7ac3e65e3f676a87f14bca7fd674
greengypsy <31978630+greengypsy@users.noreply.github.com>
2022-12-22 12:03:14 +1030

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ var express = require('express');
 
 // Ready up Express so we can start using it
 var app = express();
+
+var exphbs = require('express-handlebars');
+
+// Register '.handlebars' extension with exphbs
+app.engine('handlebars', exphbs());
+// Set our default template engine to "handlebars"
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+  res.render('header');
+});
